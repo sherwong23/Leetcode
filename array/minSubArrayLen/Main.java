b29ddb4fbd034219dbc41224112b813354343dd6
wlr <sherwong.sjtu@gmail.com>
2025-04-12 11:22:52 +0900

    minSubArrayLen

---
+++ array/minSubArrayLen/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args) {
+                Solution sol = new Solution();
+                int[] nums = {2,3,1,5,7};
+                int s = 7;
+                System.out.println(sol.minSubArrayLen(s, nums));
+		System.out.println(5);
+        }
+}
