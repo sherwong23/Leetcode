110469311b0a8bce70c116cc66f34c8d438039b5
sherwong23 <sherwong.sjtu@gmail.com>
2025-05-17 19:41:02 +0900

    1

--- backtracking/subSquences/subSquences.cpp
+++ backtracking/subSquences/subSquences.cpp
@@ -13,4 +13,26 @@ private:
         }
 
         for (int i = startIndex; i < nums.size(); i++) {
-            if (nums[i] == path.:)
+            if ((!path.empty() && nums[i] < path.back())
+                    || uset.find(nums[i]) != uset.end()) {
+                continue;
+            }
+            uset.insert(nums[i]);
+            path.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        result.clear();
+        path.clear();
+        backtracking(nums, 0);
+        return result;
+    }
+};
+
+int main() {
+}
+
+
